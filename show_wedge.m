61a81042010652099e3222003083048d3c77293d
Laura Culp <laura@culp.ca>
2013-04-26 15:10:47 -0400

    Update show_wedge to show the wedge visible on the earth, given the ISS location, next location, and camera offset from axis

--- show_wedge.m
+++ show_wedge.m
@@ -1,38 +1,61 @@
-function ret = show_wedge(ISS_position, next_frame, degrees_off_normal)
+function ret = show_wedge(ISS_position, next_frame, degrees_off_ISS_axis)
 
 % ISS_position given in [lat, lon, altitude] (altitude in meters)
 % orbit dir given in [lat,lon] of next frame
 
 % Calculate horizon
-R = earthRadius('meters')
+R = earthRadius('kilometers');
 %	at the horizon, the surface is tangent to the viewing direction (pi/2 angle)
 %	thus, (distance(center of earth to ISS)^2 
 %		   - distance(center of earth to surface)^2)^0.5
 %		= distance(ISS to horizon)
-d_ISS_horiz = sqrt( (R + ISS_position(3))^2 + R^2 );
+d_ISS_horiz = sqrt( (R + ISS_position(3)).^2 - R.^2 );
 % 	then, we can use the sine law to calculate the angle between the horizon and
 %	the ISS
-alpha = arcsin( d_ISS_horiz * sin(pi/2) / (R + ISS_position(3)) );
+alpha = asin( d_ISS_horiz * sin(pi/2) / (R + ISS_position(3)) );
 %	then, get the distance on the earth
-d_horiz = alpha * R;
+d_horiz = real(alpha) * R;
 
 % Find a vector representing the direction of the ISS
 ISS_dir(1) = next_frame(1) - ISS_position(1);
 ISS_dir(2) = next_frame(2) - ISS_position(2);
-ISS_latlon = ISS_position(1:2);
+ISS_latlon = [ISS_position(1); ISS_position(2)];
 
-% Find the direction of view
-view_dir = 10;
+% Find the unit direction of view
+dpos = [ISS_dir(1); ISS_dir(2)];
+dpos = dpos / norm(dpos);
 
-% We somehow get the viewing direction
-
+% Create a rotation matrix for the offset
+ROT = [sin(degrees_off_ISS_axis) -cos(degrees_off_ISS_axis); cos(degrees_off_ISS_axis) sin(degrees_off_ISS_axis)];
+% Apply rotation matrix to get the approximate viewing direction
+dview = ROT * dpos;
 
 % Find the wedge based on the field of view (in degrees)
-camera_fov = 1.4; % in radians
+camera_fov = 60;
+camera_fov = degtorad(camera_fov);
+% Divide the field of view in half to get how much we should rotate the viewing dir each way
+fov_half = camera_fov/2;
+% Rotate by fov_half to get the left, and -fov_half to get the right
+ROT = [sin(fov_half) -cos(fov_half); cos(fov_half) sin(fov_half)];
+dedge_l = ROT * dview;
+ROT = [sin(-fov_half) -cos(-fov_half); cos(-fov_half) sin(-fov_half)];
+dedge_r = ROT * dview;
 
 % Find the points on the horizon at the edges of the field of view
-left_top_pt = [2 4];
-right_top_pt = [5 3];
+% We are given the length in meters that we want to go, and the direction
+% We apprixomate by just adding a fraction of the direction
+temp = [ISS_latlon(1) + dedge_l(1) ISS_latlon(2) + dedge_l(2)];
+mult_dist = m_lldist( [ISS_latlon(2) temp(2)], [ISS_latlon(1) temp(1)] );
+dist_left = d_horiz / mult_dist;
+
+temp = [ISS_latlon(1) + dedge_r(1) ISS_latlon(2) + dedge_r(2)];
+mult_dist = m_lldist( [ISS_latlon(2) temp(2)], [ISS_latlon(1) temp(1)] );
+dist_right = d_horiz / mult_dist;
+
+left_top_pt = dedge_l * dist_left + ISS_latlon;
+right_top_pt = dedge_r * dist_right + ISS_latlon;
+
+% TODO: check this!
 
 % Get the max latitude, min latitude, min longitude, max longitude
 min_lat = min([ISS_latlon(1) left_top_pt(1) right_top_pt(1)]);
@@ -41,23 +64,32 @@ min_lon = min([ISS_latlon(2) left_top_pt(2) right_top_pt(2)]);
 max_lon = max([ISS_latlon(2) left_top_pt(2) right_top_pt(2)]);
 
 % Set up the projection
-m_proj( 'oblique', 'lat', [min_lat max_lat], 'lon', [min_lon max_lon], 'aspect', 1 );
+%m_proj( 'oblique', 'lat', [min_lat max_lat], 'lon', [min_lon max_lon], 'aspect', 1 );
+figure
+worldmap( [min_lat max_lat], [min_lon max_lon]);
 
 % Plot the contours
-m_coast('patch', [.6 .6 .6]);
+%m_coast('patch', [.6 .6 .6]);
+%load coast;
+%plotm(lat, long);
+geoshow('landareas.shp', 'FaceColor', [0.5 0.5 0.5]);
 
 % Plot the grid
-m_grid('box', 'fancy', 'tickdir', 'in');
+%m_grid('box', 'fancy', 'tickdir', 'in');
 
 % Plot lines between the ISS [lat lon] to these points to indicate the 
 % view wedge
-plotm(ISS_latlon, left_top_pt);
-plotm(ISS_latlon, right_top_pt);
+plotm([ISS_latlon(1) left_top_pt(1)], [ISS_latlon(2) left_top_pt(2)],'g');
+plotm([ISS_latlon(1) right_top_pt(1)], [ISS_latlon(2) right_top_pt(2)], 'r');
 
 % Calculate where the aurora is
 % We know where the aurora is in the image... we can calculate the
 % 'midpoint' very roughly...
 % Given this midpoint, and radius of circle, we need to calculate where the
+% 
+
+% Calculate where the horizon is... the fraction of black to white when cutting
+% out the earth
 % 
 
 
