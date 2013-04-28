edcb3a67b389f295f77118b4c656d694c34bce24
Andre Recnik <andre@area256.com>
2013-04-28 15:43:49 -0400

    Changed star trail code to mark the start and end of the tails

--- startrails.m
+++ startrails.m
@@ -19,16 +19,30 @@ result = zeros(nx,ny);
 % is taken at a one second interval, the trails are effectly
 % a 1 minute arc.
 for i = 11:70
-    star_extract = extractstars(strcat('../resized/ISS030-E-534',  num2str(i) , '.jpg'));
+    % Starting star locations are panted with a value of 1
+    % middle star locations are painted with a value of 100
+    % and end star locations are painted with a value of 10000
+    % So even if we have overlapping stars start stars shouldn't get 50
+    % 100, and middle stars shouldn't get above 5000, meaning we can
+    % tell the start middle and end apart.
+    if i > 10 && i < 20
+        star_value = 1;
+    elseif i > 60 && i <= 70
+        star_value = 10000; 
+    else
+        star_value = 100;
+    end
+        
+    star_extract = extractstars(strcat('../resized/ISS030-E-534',  num2str(i) , '.jpg'), star_value);
     
-    result = result + star_extract/60;
+    result = result + star_extract;
     
     display(strcat('Added image: ', num2str(i-10), 'of 60'));
 end
 
 % This median filter removes any hot pixles that made it into the
 % stacked star trails.
-filteredresult = medfilt2(result,[2,2]);
+filteredresult = double(medfilt2(result,[2,2]));
 
-imshow(filteredresult, [0,10]);
+imshow(filteredresult, [0,1000]);
 
