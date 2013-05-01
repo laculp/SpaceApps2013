557f5a45aa563722a047d6252fded7b82e357876
Andre Recnik <andre@area256.com>
2013-05-01 15:53:13 -0400

    update star trail extraction code to mark end points

--- extractstars.m
+++ extractstars.m
@@ -1,45 +1,47 @@
 %
-% Input: A file name to read.
+% Input: img_name: A file name to read.
+%        value: the value for the star to have.
+%        max_row: The maximum row to consider starting from the top of the image.
+%                 zero means do full image.
+%        resize_factor: How much to resize the image before processing it.
 % Output: A greyscale image with only points which are
 % brighter than the surrounding background (i.e. the bright stars).
 % The values are the difference between the point average and the
 % background. 
-function [ stars ] = extractstars( img_name )
+function [ stars ] = extractstars( img_name, value, max_row, resize_factor )
 
-    img = double(imread(img_name));
+    img = double(rgb2gray(imread(img_name)));
 
-    img2 = img(:,:,1);
-
-    [nx, ny, depth] = size(img2);
-
-    % Compute 3*3 and 9*9 windowed averages for each pixel location.
-    mean33 = zeros(nx, ny);
-    mean99 = zeros(nx, ny);
-
-    % This could likely be made faster by convolution with a box filter in
-    % the frequency domain.
-    for i = 11:(nx-11)
-       for j = 11:(ny-11)
-           mean33(i,j) = sum(sum(img2(i-1:i+1, j-1:j+1)))/9;
-           mean99(i,j) = sum(sum(img2(i-3:i+3, j-3:j+3)))/49;
-       end    
+    % Resize image to improve speed.
+    img = imresize(img, resize_factor);
+    
+    [nx ny depth] = size(img);
+    
+    if max_row == 0
+        max_row = nx*resize_factor;
     end
 
     % Threshold between the 9*9 window average and the 3*3 window average. 
     thress = 30;
 
+    % Compute 3*3 and 9*9 windowed averages for each pixel location.
+    mean33 = zeros(nx, ny);
+    mean99 = zeros(nx, ny);
+
     stars = zeros(nx, ny);
 
-    % Require that the 3*3 windowed mean be larger than the 9*9 windowed
-    % mean in both the current pixel, and one to the right and one down.
-    % This takes out single bright pixels, which are often noise or dimmer
-    % starts.
-    for i = 11:(nx-11)
-        for j = 11:(ny-11)
-            if (mean33(i,j) > (mean99(i,j)+thress)) && (mean33(i,j+1) > (mean99(i,j+1)+thress)) && (mean33(i+1,j) > (mean99(i+1,j)+thress))
-                stars(i,j) = mean33(i,j)-mean99(i,j);
-            end
-        end
+    % This could likely be made faster by convolution with a box filter in
+    % the frequency domain.  However box filters have strange properties in the
+    % frequency domain, so we will do this the long way to be safe for now.
+    for i = 5:min((nx-5),max_row)
+       for j = 5:(ny-5)
+           mean33(i,j) = sum(sum(img(i-1:i+1, j-1:j+1)))/9;
+           mean99(i,j) = sum(sum(img(i-3:i+3, j-3:j+3)))/49;
+
+           if (mean33(i,j) > (mean99(i,j)+thress))
+               stars(i,j) = value;
+           end
+       end    
     end
 
 end
